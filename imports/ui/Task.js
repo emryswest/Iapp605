d3a37b86a2529c01d30eb30e9ea94b4a5ad553e2
Emrys West <westemrys@gmail.com>
2018-11-18 16:16:09 -0500

    files in list, delete but no check

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,40 +1,61 @@
 import React, { Component } from 'react';
-import { Tasks } from '../api/tasks.js';
+import PropTypes from 'prop-types';
+import { UserFiles } from '../api/tasks.js';
+
+class IndividualFile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+
+    this.removeFile = this.removeFile.bind(this);
+    this.toggleChecked = this.toggleChecked.bind(this);
+
+  }
+
+  propTypes: {
+    fileName: PropTypes.string.isRequired,
+    fileSize: PropTypes.number.isRequired,
+    fileUrl: PropTypes.string,
+    fileId: PropTypes.string.isRequired
+  }
+
+  removeFile(){
+    UserFiles.remove(this.props.fileId);
+    }
 
 
-// Task component - represents a single todo item
-export default class Task extends Component {
   toggleChecked() {
   // Set the checked property to the opposite of its current value
-  Tasks.update(this.props.task._id, {
-    $set: { checked: !this.props.task.checked },
+  UserFiles.update(this.props.fileId, {
+    $set: { checked: !this.props.fileId.checked },
   });
 }
 
-deleteThisTask() {
-  Tasks.remove(this.props.task._id);
-}
-
   render() {
-    // Give tasks a different className when they are checked off,
-// so that we can style them nicely in CSS
-const taskClassName = this.props.task.checked ? 'checked' : '';
+
+const taskClassName = this.props.fileId.checked ? 'checked' : '';
 
     return (
       <li className={taskClassName}>
-        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+        <button className="delete" onClick={this.removeFile}>
           &times;
         </button>
 
         <input
           type="checkbox"
           readOnly
-          checked={!!this.props.task.checked}
-          onClick={this.toggleChecked.bind(this)}
+          checked={!!this.props.fileId.checked}
+          onClick={this.toggleChecked}
         />
 
-        <span className="text">{this.props.task.text}</span>
-      </li>      
+        <span className="text">
+          <a href={this.props.fileUrl} className="btn btn-outline btn-primary btn-sm"
+           target="_blank"><strong>{this.props.fileName}</strong></a>
+        </span>
+      </li>
     );
   }
 }
+export default IndividualFile;
