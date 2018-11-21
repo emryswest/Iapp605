004cdf0ce56802137322e20dc6905e07f7c9e953
Emrys West <westemrys@gmail.com>
2018-11-20 20:55:18 -0700

    user login and toggle complete

--- client/main.js
+++ client/main.js
@@ -3,14 +3,15 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import './main.html';
 
-//import App from '../imports/ui/App.js';
-
-//Meteor.startup(() => {
-//  render(<App />, document.getElementById('render-target'));
-//});
-
-import FileUpload from '../imports/ui/FileUpload.js';
+import '../imports/startup/accounts-config.js';
+import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
-  render(<FileUpload />, document.getElementById('render-target'));
+  render(<App />, document.getElementById('render-target'));
 });
+
+//import FileUpload from '../imports/ui/FileUpload.js';
+
+//Meteor.startup(() => {
+//  render(<FileUpload />, document.getElementById('render-target'));
+//});
